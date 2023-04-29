2abd9c5486b7d040b99750245c0cea432ebb9897
Cory Roberts <mail@coryroberts.net>
2023-04-29 13:06:02 -0400

    Commit

--- components/Feature.js
+++ components/Feature.js
@@ -23,7 +23,7 @@ const Feature = () => {
         <ScrollAnimationWrapper className="flex w-full justify-end">
           <motion.div className="h-full w-full p-4" variants={scrollAnimation}>
             <Image
-              src="/assets/Illustration2.png"
+              src="/assets/donate-group.jpg"
               alt="VPN Illustrasi"
               layout="responsive"
               quality={100}
