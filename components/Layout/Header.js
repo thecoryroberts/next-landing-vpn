05f32af615e6bc98af10aa767d50e50ef33c637c
Cory Roberts <mail@coryroberts.net>
2023-04-29 13:29:32 -0400

    Test HeaDER

--- components/Layout/Header.js
+++ components/Layout/Header.js
@@ -23,8 +23,9 @@ const Header = () => {
       >
         <nav className="max-w-screen-xl px-6 sm:px-8 lg:px-16 mx-auto grid grid-flow-col py-3 sm:py-4">
           <div className="col-start-1 col-end-2 flex items-center">
-            <LogoVPN className="h-8 w-auto" />
-          </div>
+          <div class="h-8 w-auto"><a href="/" class="text-lg"><span class="font-bold text-slate-800">Give</span><span class="text-slate-500">Comfort, Inc.</span>
+        </a></div>
+                 </div>
           <ul className="hidden lg:flex col-start-4 col-end-8 text-black-500  items-center">
             <LinkScroll
               activeClass="active"
