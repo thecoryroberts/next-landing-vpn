b7ec411b7eac82e8c67c38fbb40e6b0ff56bee15
Cory Roberts <mail@coryroberts.net>
2023-04-29 12:59:07 -0400

    next-landing-gci

--- components/Hero.js
+++ components/Hero.js
@@ -37,19 +37,18 @@ const Hero = ({
             variants={scrollAnimation}>
             <div className=" flex flex-col justify-center items-start row-start-2 sm:row-start-1">
               <h1 className="text-3xl lg:text-4xl xl:text-5xl font-medium text-black-600 leading-normal">
-                Want anything to be easy with <strong>LaslesVPN</strong>.
+               What is  Give<strong>Comfort, Inc.?</strong>.
               </h1>
               <p className="text-black-500 mt-4 mb-6">
-                Provide a network for all your needs with ease and fun using
-                LaslesVPN discover interesting features from us.
+              We are here to make sure that all of our neighbors have the items they need to feel more comfortable while they are on cancer treatment. Cancer is hard enough. We do not want anyone to go through treatment without the items they need to be more comfortable. We are here. We will give comfort.
               </p>
-              <ButtonPrimary>Get Started</ButtonPrimary>
+              <ButtonPrimary>Learn More</ButtonPrimary>
             </div>
             <div className="flex w-full">
               <motion.div className="h-full w-full" variants={scrollAnimation}>
                 <Image
-                  src="/assets/Illustration1.png"
-                  alt="VPN Illustrasi"
+                  src="/assets/donation-box.jpg"
+                  alt="Donation Box"
                   quality={100}
                   width={612}
                   height={383}
