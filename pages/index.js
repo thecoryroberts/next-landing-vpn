a3ee381e4b85207f8d3aac75cfedfc65a477f3c3
Cory Roberts <mail@coryroberts.net>
2023-04-29 13:06:23 -0400

    Title

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ import SeoHead from "../components/SeoHead";
 export default function Home() {
   return (
     <>
-      <SeoHead title='LaslesVPN Landing Page' />
+      <SeoHead title='Give Comfort, Inc.' />
       <Layout>
         <Hero />
         <Feature />
