5014a6e70e03919862acfe221409f47f0df0e672
Cory Roberts <mail@coryroberts.net>
2023-04-29 17:05:54 -0400

    Teahs

--- components/Layout/Footer.js
+++ components/Layout/Footer.js
@@ -8,7 +8,8 @@ const Footer = () => {
     <div className="bg-white-300 pt-44 pb-24">
       <div className="max-w-screen-xl w-full mx-auto px-6 sm:px-8 lg:px-16 grid grid-rows-6 sm:grid-rows-1 grid-flow-row sm:grid-flow-col grid-cols-3 sm:grid-cols-12 gap-4">
         <div className="row-span-2 sm:col-span-4 col-start-1 col-end-4 sm:col-end-5 flex flex-col items-start ">
-          <LogoVPN className="h-8 w-auto mb-6" />
+        <div class="h-8 w-auto mb-6"><a href="/" class="text-lg"><span class="font-bold text-slate-800">Give</span><span class="text-slate-500">Comfort, Inc.</span>
+        </a></div>
           <p className="mb-4">
             <strong className="font-medium">Give Comfort, Inc.</strong> is WV nonprofit serving cancer patients within our community
           </p>
