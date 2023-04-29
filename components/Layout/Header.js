d79244aaf09bca58661d4fce77552a3d42999e38
Cory Roberts <mail@coryroberts.net>
2023-04-29 17:21:00 -0400

    Header

--- components/Layout/Header.js
+++ components/Layout/Header.js
@@ -213,6 +213,38 @@ const Header = () => {
             </LinkScroll>
             <LinkScroll
               activeClass="active"
+              to="wishlist"
+              spy={true}
+              smooth={true}
+              duration={1000}
+              onSetActive={() => {
+                setActiveLink("wishlist");
+              }}
+              className={
+                "mx-1 sm:mx-2 px-3 sm:px-4 py-2 flex flex-col items-center text-xs border-t-2 transition-all " +
+                (activeLink === "wishlist"
+                  ? "  border-orange-500 text-orange-500"
+                  : " border-transparent ")
+              }
+            >
+              <svg
+                className="w-6 h-6"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                />
+              </svg>
+              Team
+            </LinkScroll>
+            <LinkScroll
+              activeClass="active"
               to="testimoni"
               spy={true}
               smooth={true}
